dd58566d6758bb90dc0f1a84460f41542d036782
Roksolana Kryvulia <151508080+roxaaaaa@users.noreply.github.com>
2025-01-04 00:26:49 +0000

    Update SwingMenu.java

--- src/ie/lyit/hotel/SwingMenu.java
+++ src/ie/lyit/hotel/SwingMenu.java
@@ -6,33 +6,23 @@ import javax.swing.*;
 
 import ie.lyit.serialize.EmployeeSerializer;
 
-// JFrame that contains two panels, one to the LEFT of the JFrame, and one to the CENTER
-// SwingMenu IS-A JFrame ==> Inheritance
 public class SwingMenu extends JFrame{ 
-	// Instance Variables go here (properties of our JFrame)...
     private static final int FRAME_WIDTH = 300;
     private static final int FRAME_HEIGHT = 300;     
 	
-	// It has two JPanels...
-    //    centerPAnel with a JLabel, and...
     private JPanel centerPanel;
     private JLabel jlblHotel;	
 	private Font f;
 	
-	//    buttonPanel with 6 JButtons
 	private JPanel buttonPanel;  
     private JButton jbtAdd, jbtList, jbtView, jbtEdit, jbtDelete, jbtExit;
 	
-    // BookSerializer object for calling methods in BookSerializer class
     private EmployeeSerializer employeeSerializer;
     
 	// Constructor - SetLayout & Add Components here...
     public SwingMenu(){	   
 	   f = new Font("Gill Sans MT", Font.BOLD, 22);		
 	   
-	   /////
-	   // Build centerPanel 
-	   /////
 	   centerPanel = new JPanel(new BorderLayout());
 	   centerPanel.setBackground(Color.WHITE);
 	   jlblHotel = new JLabel("EMPLOYEE CATEGORY",new ImageIcon("image/workforce_16864284.png"),SwingConstants.CENTER);
@@ -46,9 +36,6 @@ public class SwingMenu extends JFrame{
 	   centerPanel.add(jlblHotel, BorderLayout.CENTER);
        centerPanel.setBackground(Color.WHITE);
 
-	   /////
-       // Build buttonPanel
-       /////
        buttonPanel = new JPanel();
        buttonPanel.setLayout(new GridLayout(6,1,5,5));
        buttonPanel.setBackground(Color.WHITE);
@@ -60,7 +47,6 @@ public class SwingMenu extends JFrame{
        buttonPanel.add(jbtDelete=new JButton("DELETE"));	
        buttonPanel.add(jbtExit=new JButton("EXIT"));				
 
-		// setMnemonics on JButtons
        jbtAdd.setMnemonic('A');
        jbtList.setMnemonic('L');
        jbtView.setMnemonic('V');
@@ -68,16 +54,12 @@ public class SwingMenu extends JFrame{
        jbtDelete.setMnemonic('D');
        jbtExit.setMnemonic('X');
 
-		// Set up Frame
         add(centerPanel, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.WEST);
 
- 	    // Create a BookSerializer object to call add(), list(), etc. methods
         employeeSerializer=new EmployeeSerializer();
-        // deserializeBooks() before the JFrame is displayed...
         employeeSerializer.deserializeEmployees();
-        
-        // Create a Listener for each of the 6 JButtons		
+        	
 		jbtAdd.addActionListener(e -> {
 			employeeSerializer.add();
 		});
@@ -103,21 +85,10 @@ public class SwingMenu extends JFrame{
   
    public static void main(String[] args){ 
 	   SwingMenu frame = new SwingMenu();
-		
-		// Do whatever to frame object
-		// Set frame's title to "Three Colors Viewer"
 		frame.setTitle("EMPLOYEE CATEGORY:");
-
-		// Make frame big enough to hold components
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-
-		// Add the Default Close operation to frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		// Center the frame
         frame.setLocationRelativeTo(null);
-
-		// Set frame visible
 		frame.setVisible(true);
    }
 }
